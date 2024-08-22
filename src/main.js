c4e8a3744ff7221857b4a21a5ed2adfea632a985
rekto167 <ekysr99@gmail.com>
2024-08-22 23:50:56 +0700

    push admin lte, still not work

--- src/main.js
+++ src/main.js
@@ -1,4 +1,35 @@
-import './assets/main.css'
+// Import css
+import './assets/plugins/fontawesome-free/css/all.min.css'
+// <link
+//     rel="stylesheet"
+//     href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"
+// />
+import './assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css'
+import './assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css'
+import './assets/plugins/jqvmap/jqvmap.min.css'
+import './assets/dist/css/adminlte.min.css'
+import './assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css'
+import './assets/plugins/daterangepicker/daterangepicker.css'
+import './assets/plugins/summernote/summernote-bs4.min.css'
+import './assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css';
+// Import js
+import './assets/plugins/jquery/jquery.min.js'
+import './assets/plugins/jquery-ui/jquery-ui.min.js'
+import './assets/dist/js/demo.js'
+import './assets/plugins/bootstrap/js/bootstrap.bundle.min.js'
+import './assets/plugins/chart.js/Chart.min.js'
+import './assets/plugins/sparklines/sparkline.js'
+import './assets/plugins/jquery-knob/jquery.knob.min.js'
+import './assets/plugins/moment/moment.min.js'
+import './assets/plugins/daterangepicker/daterangepicker.js'
+import './assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js'
+import './assets/plugins/summernote/summernote-bs4.min.js'
+import './assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js'
+import './assets/dist/js/adminlte.js'
+import './assets/dist/js/pages/dashboard.js'
+// JQVMap
+import './assets/plugins/jqvmap/jquery.vmap.min.js'
+import './assets/plugins/jqvmap/maps/jquery.vmap.usa.js'
 
 import { createApp } from 'vue'
 import App from './App.vue'
---
+++ src/pages/Jalur/List.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+    msg: {
+        type: String,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <div>
+        <h1>List Jalur</h1>
+    </div>
+</template>
